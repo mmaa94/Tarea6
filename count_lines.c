a566fad8c0eb93540ea7947274cdc6b0beb60bcf
mmaa94 <maggi123_940620@hotmail.com>
2013-10-27 20:08:57 -0500

    Arregladas las etiquetas

--- count_lines.c
+++ count_lines.c
@@ -10,6 +10,11 @@ Fecha de creación: Oct  24 03:14:34 COT 2013
 
 int count_lines(char *filename){
 
+  /**
+   Input: NOmbre del archivo en el que se quiere contar el número de líneas
+   Output: Número de líneas
+  **/
+
 	FILE *in;
 	int n_lineas;
 	int c;
