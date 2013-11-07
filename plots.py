18854b223ff4a79abdb990986a7184927d525f31
Nagudelo <nagudelo@ubuntu.(none)>
2013-11-07 12:54:16 -0500

    bla

--- plots.py
+++ plots.py
@@ -1,28 +1,28 @@
 import pylab
 import numpy
 
-#El programa grafica la evolución de la galaxia en 5000 millones de años. 
-#Autores: Nathalie Agudelo Dueñas y  María M. Ariza Acero 
-#Fecha de creación: Oct/24/2013
+#El programa grafica la evolucion de la galaxia en 5000 millones de anos. 
+#Autores: Nathalie Agudelo y  Maria M. Ariza Acero 
+#Fecha de creacion: Oct/24/2013
 
-#En este recorrido se extraen los datos de evolución de la galaxia cada 5000 millones de años
+#En este recorrido se extraen los datos de evolucion de la galaxia cada 5000 millones de anos
 
 for i in range(5):
 
-#Leyendo información de los archivos generados en evolve.c
+#Leyendo informacion de los archivos generados en evolve.c
 
-    data_ev = numpy.loadtxt(open('evolution'+str(i)+'.txt', 'r'))
+    data_ev = numpy.loadtxt(open('evolution'+str(i+1)+'.txt', 'r'))
     x = data_ev[:,1]
     y = data_ev[:,2]
 
 #Graficando las posiciones de las estrellas en un tiempo t
-#Guardando gráficas en archivos .jpg    
+#Guardando graficas en archivos .jpg    
     
     pylab.plot(x,y)
-    pylab.title('Evolución de la galaxia en 5000 millones de años')
+    pylab.title('Evolucion de la galaxia en 5000 millones de anos')
     pylab.xlabel('x(t)')
     pylab.ylabel('y(t)')
-    pylab.savefig('evolution'+str(i)+'.jpg')
+    pylab.savefig('evolution'+str(i+1)+'.jpg')
     pylab.close()
     
  
